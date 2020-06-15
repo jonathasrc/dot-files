35c1ea1175f07bb2ca2a5207a7d65f041e251382
Jonathas Rodrigues <jonathasrc96@gmail.com>
2020-06-15 01:26:42 -0300

    Refactor(.gitignore) add file teste.sh

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-#  dot-files
+# DOT-FILES
 
-### List app installed in environment.sh
-* Asdf [https://asdf-vm.com/#/][Asdf-vm Manage multiple runtime
-    versions]
-* Vagrant[https://www.vagrantup.com/docs/installation/source.html][Vagrant
-    coc]
+## List app installed in environment.sh
+
+* Asdf [https://asdf-vm.com/#/] [Asdf-vm Manage multiple runtime versions]
+* Vagrant [https://www.vagrantup.com/docs/installation/source.html] [Vagrant coc]
+* Spotify [https://www.spotify.com/br/download/linux/] [Spotify]
+* PyENV [https://github.com/pyenv/pyenv] [PyENV]
+* Docker [https://docs.docker.com/engine/install/ubuntu/] [Docker]
--- environment.sh
+++ environment.sh
@@ -1,42 +1,35 @@
-#bin/bash
-#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
-#app falta colocar a instalacao
-# vagrant
-# asdf
+#!/bin/bash
 # virtualbox
 
-app[0]="git-all"
-app[1]="neovim"
-app[2]="tmux"
-app[3]="tmuxinator"
-app[4]="spotify-client"
 
-libs[0]="apt-transport-https"
-libs[1]="ca-certificates"
-libs[2]="curl"
-libs[3]="gnupg-agent"
-libs[4]="software-properties-common"
+# Leia e instale libs do arquivo libs.txt 
+# Read and install libs from the libs.txt file
+cat ./libs.txt | while read libs; do
+    apt install $libs
+done
 
-apt install ${libs[*]}
+# Leia e instale aplicaticos do arquivo app.txt
+# Read and install aplicativos from the app.txt file
+cat ./app.txt | while read app; do
+    apt install $app
+done
 
-docker[0]="docker-ce"
-docker[1]="docker-ce-cli"
-docker[2]="containerd.io"
 
-dockerr[0]="docker"
-dockerr[1]="docker-engine"
-dockerr[2]="docker.io"
-dockerr[3]="containerd"
-dockerr[4]="runc"
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
 
- apt remove ${dockerr[*]}
- apt autoremove
 
-curl https://pyenv.run | bash
-
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+# Install  spotify
+CURL -Ss HTTPS://DOWNLOAD.SPOTIFY.COM/DEBIAN/PUBKEY.GPG | SUDO APT-KEY ADD - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
+
+# Install PYENV
+curl https://pyenv.run | bash
+
+# Install Docker Engine
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
  apt-key fingerprint 0EBFCD88
  add-apt-repository \
@@ -46,34 +39,23 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 
 apt update
 apt install docker-ce docker-ce-cli containerd.io -y
+
+# Install Docker Compose
  curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
  chmod +x /usr/local/bin/docker-compose
  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 
-if ! apt install ${app[*]} -y
-then 
-   echo "UNABLE INSTALL APP"
-fi
-# INSTALL VAGRANT
-
-git clone https://github.com/hashicorp/vagrant.git
-sudo cp -r vagrant /etc/opt/
-cd /etc/opt/vagrant/
-bundle install # ruby precisa está instalado
-ln -sf ./exec/vagrant /usr/local/bin/vagrant
-cd 
-# INSTALL NVM
-
+# Install NVM
  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
  
  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
  nvm install node
 
-# INSTALLL VIM PLUG
 
-#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+# InstallL Vim Plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
  source ~/.bashrc
 
